8ad08b5914bc8db13ff8e024528a254e8e7fb94c
PratikBisne <100847658+PratikBisne@users.noreply.github.com>
2022-03-03 18:07:33 +0530

    Add files via upload

---
+++ scan1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ class scan1
+ {
+	 public static void main(String args[])
+	 {
+		Scanner sc = new Scanner("25 65");
+		// System.out.println("Enter num1");
+			int i=sc.nextInt();
+			
+		// System.out.println("Enter num2");
+			 int j=sc.nextInt();
+			
+		System.out.println("Sum of num1 & num2 is "+(i+j));
+		
+		
+		// System.out.println("Enter num1");
+			// float f =sc.nextFloat();
+			
+		// System.out.println("Enter num2");
+			// int k =sc.nextInt();
+			
+		// System.out.println("Division will be "+(f/k));
+		
+		// System.out.println("Enter num1");
+			// float f1 = sc.nextFloat();
+		// System.out.println("Enter num2");
+			// int p = sc.nextInt();
+		// System.out.println("Multiplication will be "+(f1*p));	
+			
+		
+		
+	 }
+	 
+	 
+ }
